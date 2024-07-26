d6918046d2546db2865019249b2d2ddecc0651bd
Santiago Rodriguez <santiara010@gmail.com>
2024-07-26 18:36:38 -0300

    Hice el punto 2

--- js/Pto-2.js
+++ js/Pto-2.js
@@ -1,11 +1,22 @@
-const edad = parseInt(prompt("Ingrese su edad"));
+const nota = parseInt(prompt("Ingrese nota numerica"));
 
-if (edad > 0 && edad < 120 ) {
-    if(edad > 18){
-        document.write(`<p> Tu edad es ${edad}, puedes conducir </p>`) 
-    } else{
-        document.write(`<p> Tu edad es ${edad}, no puedes conducir </p>`) 
+if (nota >= 0 && nota <= 10 ) {
+    if(nota >= 0 && nota <= 2){
+        alert(`Tu nota es ${nota}, es decir, muy deficiente`) 
+    }  else if (nota > 2 && nota <= 4){
+        alert(`Tu nota es ${nota}, es decir, insuficiente`) 
+    }  else if (nota > 4 && nota < 7){
+        alert(`Tu nota es ${nota}, es decir, suficiente`) 
+    } 
+    else if (nota == 7){
+        alert(`Tu nota es ${nota}, es decir, bien`) 
+    } else if (nota > 7 && nota <= 9){
+        alert(`Tu nota es ${nota}, es decir, notable`) 
+    } else if (nota == 10){
+        alert(`Tu nota es ${nota}, es decir, sobresaliente`) 
     }
-    } else{
-        document.write(`<p> Ingrese una edad válida </p>`) 
-    }
+} else if (nota > 10 || nota < 0 ){
+    document.write(`<p> número erroneo </p>`)  
+} else {
+    document.write(`<p> Ingrese un número válido </p>`)
+}
